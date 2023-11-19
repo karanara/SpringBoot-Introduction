5f4a529b3183ee4e2c2d6409c166ae702dbe5dcb
karanara <karanara@mail.uc.edu>
2023-11-19 14:34:58 -0500

    RestController Example

---
+++ src/main/java/com/practice1/springboot/rest/FunRestController.java
@@ -0,0 +1,13 @@
+package com.practice1.springboot.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+	@GetMapping("/")
+	public String sayHello() {
+		return "HelloWorld";
+	}
+}
