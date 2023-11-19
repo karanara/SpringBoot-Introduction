ab299c055d682aa76ea97c0a0201ba0993f35fe0
karanara <karanara@mail.uc.edu>
2023-11-19 18:56:49 -0500

    added custom application properties

--- src/main/java/com/practice1/springboot/rest/FunRestController.java
+++ src/main/java/com/practice1/springboot/rest/FunRestController.java
@@ -1,11 +1,22 @@
 package com.practice1.springboot.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+	//injecting properties from application.properties(coach and team name)
+	@Value("${coach.name}")
+	private String coachName;
+	@Value("${team.name}")
+	private String teamName;
 
+	//exposing teaminfo
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return "CoachName  "+ coachName + " , teamName  " + teamName;
+	}
 	@GetMapping("/")
 	public String sayHello() {
 		return "HelloWorld";
